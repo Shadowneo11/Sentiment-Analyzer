a78d54dc55ad3b54348bce6c4330d24702857656
shadowneo11 <kobywillis7@gmail.com>
2025-12-02 23:30:53 -0500

    feat: Updated Vite Config for GitHub Pages Deployment

--- sentiment-analyser/vite.config.js
+++ sentiment-analyser/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: "sentiment-analyser",
+  base: "/Sentiment-Analyzer/",
   plugins: [react()],
 })
