5106b60f7ebeda3f5159bd3adfb8411fc7f92dd1
shadowneo11 <kobywillis7@gmail.com>
2025-12-02 23:53:23 -0500

    fix: updated build script to cd into project subdirectory

--- sentiment-analyser/vite.config.js
+++ sentiment-analyser/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "sentiment-analyser",
   base: "/Sentiment-Analyzer/",
   plugins: [react()],
 })
